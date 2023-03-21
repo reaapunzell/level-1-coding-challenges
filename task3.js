307a10604c6158dece76271616f66c22a28c8276
rea <rea.maroga@gmail.com>
2023-03-21 16:28:16 +0200

    added task3

---
+++ task3.js
@@ -0,0 +1,10 @@
+function isSixtyFive(num1, num2){
+  var sum = num1 + num2;
+  if ( num1 === 65 || num2 ===65 ){
+    return "true";
+  } else if ( sum === 65 ){
+    return "true";
+  } else {
+  return "false";
+  } 
+}; module. exports = { task3 } ;
